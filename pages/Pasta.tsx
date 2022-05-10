67b87a810b246521e67fff3cfa9d1d6872a4e4bc
Jan Nicklas <j.nicklas@me.com>
2022-05-10 16:40:19 +0200

    Update Pasta.tsx

--- pages/Pasta.tsx
+++ pages/Pasta.tsx
@@ -2,7 +2,7 @@ import { PreloadedQuery } from "react-relay";
 import { graphql, usePreloadedQuery } from "react-relay/hooks";
 import { getPreloadedQuery } from "../data/network";
 import { Flag } from "../src/components/Flag";
-import PastaQuery, { Pasta_Query } from "../__generated__/Pasta_Query.graphql";
+import preloadQuery, { Pasta_Query } from "../__generated__/Pasta_Query.graphql";
 
 interface ProductProps {
   queryRefs: {
@@ -10,17 +10,15 @@ interface ProductProps {
   };
 }
 
-const query = graphql`
-  query Pasta_Query($countryCode: ID!) {
-    country(code: $countryCode) @required(action: THROW) {
-      name
-      ...Flag_icon
-    }
-  }
-`;
-
 const Pasta = (props: ProductProps) => {
-  const data = usePreloadedQuery(query, props.queryRefs.query);
+  const data = usePreloadedQuery(graphql`
+    query Pasta_Query($countryCode: ID!) {
+      country(code: $countryCode) @required(action: THROW) {
+        name
+        ...Flag_icon
+      }
+    }
+  `, props.queryRefs.query);
   return (
     <main>
       <h1>{data.country.name}</h1>
@@ -33,7 +31,7 @@ export async function getStaticProps() {
   return {
     props: {
       preloadedQueries: {
-        query: await getPreloadedQuery(PastaQuery, {
+        query: await getPreloadedQuery(preloadQuery, {
           countryCode: "IT",
         }),
       },
